13d48be28e80c0b32c7a21fd0ce8306555e58700
Abdullah <abdullahazeemi200@gmail.com>
2025-03-02 22:43:10 +0500

    Dashboard Implementation

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -4,6 +4,7 @@ type Props = {}
 
 const Row1 = (props: Props) => {
   const { data } = useGetKpisQuery(); 
+  //console.log("data", data);
   return (
    <>
     <DashboardBox gridArea="a"></DashboardBox>
--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
-dotnev.config();
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(hemlet());
+app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
 app.use(morgan("common"));
 app.use(bodyParser.json());
@@ -18,3 +21,41 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 console.log("hello");
+
+app.use("/kpi", kpiRoutes);
+
+const dbURI = process.env.MONGO_URL;
+
+mongoose.connect(dbURI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    serverSelectionTimeoutMS: 15000
+})
+.then(() => {
+    console.log('Database connected successfully');
+    app.listen(process.env.PORT || 9000, () => {
+        console.log(`Server Port ${process.env.PORT || 9000}`);
+        // insertKpis(kpis); 
+    });
+})
+.catch(err => {
+    console.error('Database connection error:', err);
+});
+
+
+
+
+
+
+
+
+
+
+// async function insertKpis(kpis) {
+//     try {
+//         await KPI.insertMany(kpis);
+//         console.log('Kpis inserted successfully');
+//     } catch (error) {
+//         console.error('Error inserting kpis:', error);
+//     }
+// }
